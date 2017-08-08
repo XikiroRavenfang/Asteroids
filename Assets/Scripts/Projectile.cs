0b928270d35062c84bee8f5160b83939b92bb2dc
XikiroRavenfang <mattsmart98@gmail.com>
2017-08-08 13:30:32 +1000

    Improved Code Comments.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    // time before shot expires
+    // Time before shot expires
     public float shotTime = 1.2f;
 
     private Rigidbody2D rigid2D;
@@ -13,13 +13,14 @@ public class Projectile : MonoBehaviour
     void Start()
     {
         rigid2D = GetComponent<Rigidbody2D>();
-        // destroy projectile after set time
+        // Destroy projectile after set time
         Destroy(gameObject, shotTime);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Rotates the projectile to match it's velocity
         Vector3 vel = rigid2D.velocity;
         float angle = Mathf.Atan2(vel.y, vel.x);
         rigid2D.rotation = angle * Mathf.Rad2Deg;
